8f3216333315c867ca9ebaa194d6a6ae7122dc6c
kraftenty <kraftenty@gmail.com>
2024-03-22 14:02:52 +0900

    test 작성법 추가

---
+++ src/test/java/org/nl/javatetris/TestTest.java
@@ -0,0 +1,17 @@
+package org.nl.javatetris;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+    @Test
+    public void simpleTest() {
+        // given
+        int age = 20;
+        // when
+
+        // then
+        Assertions.assertEquals(20, age);
+    }
+}
